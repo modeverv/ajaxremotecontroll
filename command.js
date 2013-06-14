6ef9bb640a88c7c15c035440d06de24d5a81e42a
s.ikehata <s.ikehata@qloog.com>
2013-06-15 08:12:08 +0900

    m

--- command.js
+++ command.js
@@ -1,8 +1,16 @@
 var com = { 
- "session" : "xxx",
- "command" : function(){
-   console.log("aaaaaaaaaaaaaaa");
+    session : "xcxc,xxx",
+    command : function(){
 
- }
+        if(!document.getElementById("hoge")){
+            var html = "<div id=\"hoge\" style=\"position:absolute;top:0;left:0;width:200px;\"></div>";
+            $("body").append(html);
+
+        }
+
+        var message = "a";
+        $("#hoge").html($("#hoge").html() +"<br/>" + message );
+        //$("#hoge").remove();
+    }
 };
 
--- remote.js
+++ remote.js
@@ -2,15 +2,19 @@ function evil_eval(){
     $.ajax({
                url: "./command.js",
                cache: false,
+               crossDomain : true,
+               dataType : "script",
                success: function(res){
-                   eval(res);
-                   if(com.session == SESSION)
+                   if(com.session.match(SESSION)){
                        com.command.call();
+                   }else{
+                       console.log("for other session command.");
+                   }
                }
            });    
 }
 
 var timer;
 $(function(){
-      timer = setInterval(evil_eval,1000);
+      timer = setInterval(evil_eval,5000);
   });
